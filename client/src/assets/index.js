849c129d7fb2e3bef3446c965fa20b74d44a70c0
DSAghicha <dsaghicha211@gmail.com>
2022-03-15 23:54:36 +0530

    Export Show/Hide Vectors as React Components

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,2 +1,6 @@
 export { default as Logo } from './logo.png'
 export { ReactComponent as HomeSVG } from './images/home.svg'
+
+// ICONS
+export { ReactComponent as ShowPasswordSVG } from './icons/show_password.svg'
+export { ReactComponent as HidePasswordSVG } from './icons/hide_password.svg'
