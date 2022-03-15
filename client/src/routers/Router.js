b9823fd12b7924d3850e8b8628483565ba72ec47
DSAghicha <dsaghicha211@gmail.com>
2022-03-15 19:02:17 +0530

    Added route for About page

--- client/src/routers/Router.js
+++ client/src/routers/Router.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from '../pages'
+import { Home, About } from '../pages'
 
 const Router = () => {
     return (
@@ -7,6 +7,7 @@ const Router = () => {
             <Routes>
                 {/* PUBLIC ROUTES */}
                 <Route path='/' element={ <Home /> }/>
+                <Route path='/about' element={ <About /> }/>
             </Routes>
         </BrowserRouter>
     )
